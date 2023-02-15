d83027f4b0985ddb0652e678921a860becf3da2d
codegino <carlo.gino.catapang@devoteam.com>
2023-02-15 17:36:06 +0100

    feat: Created first popup plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { crx } from "@crxjs/vite-plugin";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import manifest from "./manifest.json";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [svelte(), crx({ manifest })],
+});
